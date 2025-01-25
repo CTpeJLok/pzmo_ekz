c8611e3d3191058dff227208dd468425cd4271e5
CTpeJLok <good-i-evil@yandex.ru>
2025-01-25 18:17:48 +0300

    fix, need to fix my articles

--- app/main/urls.py
+++ app/main/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("new-session", views.new_session, name="new_session"),
     path("my", views.my_articles, name="my_articles"),
     path("load", views.load_data, name="load"),
-    path("<str:article_id>/<str:action>", views.article_action, name="article_action"),
-    path("<str:article_id>", views.article, name="article"),
+    path("action/<str:article_id>/<str:action>", views.article_action, name="article_action"),
+    path("article/<str:article_id>", views.article, name="article"),
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -1,26 +1,21 @@
 import pandas as pd
 from datetime import datetime
 from random import randint
-from scipy.sparse import csr_matrix
 
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+from django.contrib import messages
 
 from core.settings import BASE_DIR
 
 from .models import Article, UserInteract
 
+import pickle
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
 from sentence_transformers import SentenceTransformer
-
-from .ml import (
-    build_user_profile_on_the_fly,
-    recommend_items,
-    article_metadata,
-    article_embeddings,
-    bert_model_params,
-)
-
 
 EVENTS = {
     "v": "VIEW",
@@ -38,48 +33,88 @@ EVENT_WEIGHT = {
     "COMMENT CREATED": 4.0,
 }
 
-# with open(BASE_DIR / "fm_recommender_model.pkl", "rb") as f:
-#     fm_recommender_model = pickle.load(f)
+article_embeddings = np.load('article_embeddings.npy')
 
-# with open(BASE_DIR / "cf_recommender_model2.pkl", "rb") as f:
-#     cf_recommender_model = pickle.load(f)
+with open('article_metadata.pkl', 'rb') as meta_file:
+    article_metadata = pickle.load(meta_file)
 
-model = SentenceTransformer(bert_model_params["model_name"])
-embedding_dim = bert_model_params["embedding_dim"]
+with open('bert_model_params.pkl', 'rb') as params_file:
+    bert_model_params = pickle.load(params_file)
+
+# Загружаем BERT-модель
+model = SentenceTransformer(bert_model_params['model_name'])
+embedding_dim = bert_model_params['embedding_dim']
 
 
-# Функция для получения рекомендаций
-def get_recommendations(
-    user_id,
-    user_interactions,
-    embedding_dim,
-    topn=10,
-):
-    print(
-        f"User {user_id} has >=5 interactions. Generating recommendations using BERT."
-    )
+def build_user_profile_on_the_fly(user_interactions, article_metadata, article_embeddings):
+    """
+    Создает профиль пользователя на основе взаимодействий.
+    """
+    # Проверяем, пуст ли массив взаимодействий
+    if user_interactions.size == 0:
+        return np.zeros(embedding_dim)
+
+    user_embeddings = []
+    strengths = []
+    for content_id, strength in user_interactions:
+        try:
+            idx = article_metadata[article_metadata['contentId'] == content_id].index[0]
+            user_embeddings.append(article_embeddings[idx])
+            strengths.append(strength)
+        except IndexError:
+            continue  # Пропускаем, если contentId не найден
+
+    user_embeddings = np.array(user_embeddings)
+    strengths = np.array(strengths).reshape(-1, 1)
+
+    weighted_sum = np.sum(user_embeddings * strengths, axis=0)
+    return weighted_sum / np.sum(strengths)
+
+
+
+def recommend_items(user_profile, article_embeddings, article_metadata, topn=10):
+    """
+    Рекомендует статьи на основе косинусного сходства.
+    """
+    cosine_similarities = cosine_similarity(user_profile.reshape(1, -1), article_embeddings).flatten()
+    top_indices = cosine_similarities.argsort()[-topn:][::-1]
+    recommendations = article_metadata.iloc[top_indices].copy()  # Добавляем .copy() для создания новой копии DataFrame
+    recommendations.loc[:, 'similarity'] = cosine_similarities[top_indices]
+    return recommendations
+
+
+def get_recommendations(user_id, interactions_df, topn=10):
+    """
+    Рекомендации для пользователя:
+    <5 взаимодействий -> популярные статьи.
+    >=5 взаимодействий -> рекомендации через BERT.
+    """
+    user_interactions = interactions_df[interactions_df['personId'] == user_id]
+    user_interactions_count = len(user_interactions)
+
+    print(f"User {user_id} has >=5 interactions. Generating recommendations using BERT.")
     user_profile = build_user_profile_on_the_fly(
-        user_interactions[["contentId", "eventStrength"]].values,
+        user_interactions[['contentId', 'eventStrength']].values,
         article_metadata,
-        article_embeddings,
-        embedding_dim,
+        article_embeddings
     )
-    recommendations = recommend_items(
-        user_profile, article_embeddings, article_metadata, topn=topn
-    )
+    recommendations = recommend_items(user_profile, article_embeddings, article_metadata, topn=topn)
     return recommendations
 
 
 def generate_session_id():
-    id = randint(0, 9999999999999999999)
-    return "{:019d}".format(id)
+    id = randint(0, 9223372036854775807)
+    return "{:018d}".format(id)
 
 
 @login_required(login_url="login")
 def home(request):
-    articles = Article.objects.prefetch_related("interacts").order_by("-timestamp")[
-        :250
-    ]
+    # articles = Article.objects.prefetch_related("interacts").order_by("-timestamp")[
+    #     :250
+    # ]
+
+    articles = Article.objects.annotate(i_c=Count('interacts')).order_by('-i_c', '-timestamp')[:250]
+
 
     response = render(
         request,
@@ -90,6 +125,20 @@ def home(request):
     if not request.COOKIES.get("session_id"):
         response.set_cookie(key="session_id", value=generate_session_id())
 
+    return response
+
+
+@login_required(login_url='login')
+def new_session(request):
+    articles = Article.objects.annotate(i_c=Count('interacts')).order_by('-i_c', '-timestamp')[:250]
+
+    response = render(
+        request,
+        "main/home.html",
+        {"articles": articles, "user": request.user},
+    )
+    
+    response.set_cookie(key="session_id", value=generate_session_id())
     return response
 
 
@@ -141,6 +190,11 @@ def article_action(request, article_id, action):
 
 @login_required(login_url="login")
 def my_articles(request):
+    interactions_df = UserInteract.objects.filter(personId=request.user).order_by('contentId')
+    if interactions_df.count() < 5:
+        messages.add_message(request, messages.INFO, 'Вы новый пользователь, поэтому мы не можем составить для вас рекомендации')
+        return redirect('home')
+
     file_path_articles = "shared_articles.csv"
     articles_df = pd.read_csv(file_path_articles)
     articles_df = articles_df[articles_df["eventType"] == "CONTENT SHARED"]
@@ -148,7 +202,6 @@ def my_articles(request):
         columns=["authorUserAgent", "authorRegion", "authorCountry"], inplace=True
     )
 
-    interactions_df = UserInteract.objects.filter(personId=request.user)
     interactions_df = list(interactions_df.values())
     interactions_df = pd.DataFrame(interactions_df)
     interactions_df.drop(columns=["id"], inplace=True)
@@ -161,7 +214,8 @@ def my_articles(request):
             "personId_id": "personId",
         }
     )
-    print(interactions_df.head())
+
+    print(interactions_df.head(100))
 
     # Веса взаимодействий
     event_type_strength = {
@@ -178,14 +232,18 @@ def my_articles(request):
         ["personId", "contentId"], as_index=False
     ).agg({"eventStrength": "sum"})
 
+    print(interactions_df.head(100))
+  
     recommended_articles = get_recommendations(
         request.user.id,
         interactions_df,
         topn=10,
     )
-    print(recommended_articles)
+    
+    article_ids = recommended_articles["contentId"].tolist()
+    articles = Article.objects.filter(contentId__in=article_ids)
 
-    return render(request, "main/home.html", {"user": request.user})
+    return render(request, "main/home.html", {"user": request.user, 'articles': articles, 'is_rec': True})
 
 
 @login_required(login_url="login")
